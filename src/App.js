781889b4649a825edd6cadf0fc1a0aca97e584e0
woodygaragelab <woodygaragelab@gmail.com>
2021-03-07 11:45:55 +0900

    comment notes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
+//import { listNotes } from './graphql/queries';
 import { listPersons } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+//import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 import { createPerson as createPersonMutation, deletePerson as deletePersonMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
   const [persons, setPersons] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
@@ -19,18 +19,18 @@ function App() {
     fetchPersons();
   }, []);
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
+  // async function fetchNotes() {
+  //   const apiData = await API.graphql({ query: listNotes });
+  //   const notesFromAPI = apiData.data.listNotes.items;
+  //   await Promise.all(notesFromAPI.map(async note => {
+  //     if (note.image) {
+  //       const image = await Storage.get(note.image);
+  //       note.image = image;
+  //     }
+  //     return note;
+  //   }))
+  //   setNotes(apiData.data.listNotes.items);
+  // }
 
   async function fetchPersons() {
     const apiData = await API.graphql({ query: listPersons });
@@ -45,16 +45,16 @@ function App() {
     setPersons(apiData.data.listPersons.items);
   }
 
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
-    }
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
+  // async function createNote() {
+  //   if (!formData.name || !formData.description) return;
+  //   await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  //   if (formData.image) {
+  //     const image = await Storage.get(formData.image);
+  //     formData.image = image;
+  //   }
+  //   setNotes([ ...notes, formData ]);
+  //   setFormData(initialFormState);
+  // }
 
   async function createPerson() {
     if (!formData.name || !formData.description) return;
@@ -67,11 +67,11 @@ function App() {
     setFormData(initialFormState);
   }
 
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
+  // async function deleteNote({ id }) {
+  //   const newNotesArray = notes.filter(note => note.id !== id);
+  //   setNotes(newNotesArray);
+  //   await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  // }
 
   async function deletePerson({ id }) {
     const newPersonsArray = persons.filter(person => person.id !== id);
@@ -84,7 +84,8 @@ function App() {
     const file = e.target.files[0];
     setFormData({ ...formData, image: file.name });
     await Storage.put(file.name, file);
-    fetchNotes();
+    // fetchNotes();
+    fetchPersons();
   }
 
   
